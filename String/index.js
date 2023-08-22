0186ab05e8deec6ba47e9179abcac092167c37f9
shreyas bole <shreyasbole4@gmail.com>
2023-08-22 23:18:09 +0530

    string completed

---
+++ String/index.js
@@ -0,0 +1,103 @@
+// string in javascript
+
+let firstName = "shreyas";
+
+// length of string
+
+console.log(firstName.length); // 7
+
+console.log(firstName.charAt(0)); // s
+
+console.log(firstName.charCodeAt(0)); // 115 (ASCII value of s)
+
+console.log(firstName.includes("s")); // true
+
+console.log(firstName.indexOf("y")); // 4
+
+console.log(firstName.lastIndexOf("s")); // 6
+
+// compare two strings
+let anotherName = "shreyas";
+console.log(firstName.localeCompare(anotherName)); // 0 (if both are same) else -1 or 1 (if not same)
+
+// replace shreyas with atul
+const str = "shreyas is best frontend developer";
+console.log(str.replace("shreyas", "atul")); // atul is best frontend developer
+
+console.log(str.substring(6, 20)); // is best frontend
+
+console.log(str.slice(6, 20)); //  is best frontend & here we can use negative values also
+
+console.log(str.split(" ")); // ["shreyas", "is", "best", "frontend", "developer"]
+
+console.log(str.startsWith("shreyas")); // true
+
+console.log(str.trim()); // shreyas is best frontend developer & remove extra spaces from start and end
+
+console.log(str.toUpperCase()); // SHREYAS IS BEST FRONTEND DEVELOPER
+
+console.log(str.toLocaleUpperCase()); // SHREYAS IS BEST FRONTEND DEVELOPER
+
+// convert string to number
+let num = 123;
+console.log(num.toString()); // "123"
+
+// conver onject to string
+let obj = {
+  name: "shreyas",
+  age: 21,
+};
+
+console.log(obj, JSON.stringify(obj)); // {name: "shreyas", age: 21} "{"name":"shreyas","age":21}"
+
+// concat two strings
+const lastName = "bole";
+console.log(firstName.concat(" ", lastName)); // shreyas bole
+
+//TODO: question-1 : find the index of first occurence in string
+
+var strStr = function (haystack, needle) {
+  if (needle === "") return 0;
+  if (haystack.includes(needle)) {
+    return haystack.indexOf(needle);
+  } else {
+    return -1;
+  }
+};
+
+//TODO: question-2 : reverse a string
+
+var reverseString = function (s) {
+  let start = 0;
+  let end = s.length - 1;
+  while (start < end) {
+    let temp = s[start];
+    s[start] = s[end];
+    s[end] = temp;
+    start++;
+    end--;
+  }
+  return s;
+};
+
+//TODO: question-3 : valid Anaagram
+
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  let s1 = s.split("").sort().join("");
+  let t1 = t.split("").sort().join("");
+  return s1 === t1;
+}
+
+//TODO: question-4 : Longest Common Prefix
+
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+    }
+  }
+  return prefix;
+};

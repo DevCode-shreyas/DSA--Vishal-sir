b1d85cd67213d09e097430301d5b5de6e2166ee1
shreyas bole <shreyasbole4@gmail.com>
2023-08-23 23:43:10 +0530

    string question added

--- String/index.js
+++ String/index.js
@@ -101,3 +101,16 @@ var longestCommonPrefix = function (strs) {
   }
   return prefix;
 };
+
+//TODO: question-5 : Merge Strings Alternately
+
+var mergeAlternately = function (word1, word2) {
+  let result = "";
+  let i = 0;
+  while (i < word1.length || i < word2.length) {
+    if (i < word1.length) result += word1[i];
+    if (i < word2.length) result += word2[i];
+    i++;
+  }
+  return result;
+};

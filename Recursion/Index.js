b564dc75ef81b7b57ac308abf6fca555dfabfb4d
shreyas bole <shreyasbole4@gmail.com>
2023-08-29 17:10:32 +0530

    recursion

---
+++ Recursion/Index.js
@@ -0,0 +1,42 @@
+// Recursion - a function that calls itself until it doesn't (base case)
+
+// print fun n times
+// function fun(n) {
+//   if (n == 0) {
+//     return;
+//   }
+//   console.log("fun");
+//   fun(n - 1);
+// }
+
+// fun(5);
+
+// factorial of a number
+function fact(n) {
+  if (n == 0) {
+    return 1;
+  }
+  return n * fact(n - 1);
+}
+
+console.log(fact(8));
+
+// Sum of Array
+function sum(arr, n) {
+  if (n <= 0) {
+    return 0;
+  }
+  return sum(arr, n - 1) + arr[n - 1];
+}
+
+console.log(sum([1, 2, 3, 4, 5], 5));
+
+// Fibonacci numbers
+function fib(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(5));

f0dd768137479c6956a2445c00c06a8fdeddc9b8
shreyas bole <shreyasbole4@gmail.com>
2023-09-10 21:11:17 +0530

    question added

--- Object/index.js
+++ Object/index.js
@@ -126,3 +126,18 @@ const playerCount = (data) => {
 
 const countPlayer = playerCount(data);
 console.log(countPlayer);
+
+// 1512. Number of Good Pairs
+// https://leetcode.com/problems/number-of-good-pairs/
+
+var numIdenticalPairs = function (nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+};

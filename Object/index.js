7ef13b15e043d0de73ab66f4c1f2889e3353d646
shreyas bole <shreyasbole4@gmail.com>
2023-09-13 22:52:59 +0530

    q added

--- Object/index.js
+++ Object/index.js
@@ -157,3 +157,20 @@ var numberOfSteps = function (num) {
   }
   return count;
 };
+
+// 1365. How Many Numbers Are Smaller Than the Current Number
+// https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+var smallerNumbersThanCurrent = function (nums) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] < nums[i]) {
+        count++;
+      }
+    }
+    result.push(count);
+  }
+  return result;
+};

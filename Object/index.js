4f138ca6cac93d9212713a9d907ce580e8c34465
shreyas bole <shreyasbole4@gmail.com>
2023-09-14 22:18:40 +0530

    q added

--- Object/index.js
+++ Object/index.js
@@ -174,3 +174,19 @@ var smallerNumbersThanCurrent = function (nums) {
   }
   return result;
 };
+
+// 1431. Kids With the Greatest Number of Candies
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+var kidsWithCandies = function (candies, extraCandies) {
+  let result = [];
+  let max = Math.max(...candies);
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= max) {
+      result.push(true);
+    } else {
+      result.push(false);
+    }
+  }
+  return result;
+};

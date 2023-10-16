7306e3bce546ef19584043c37c8d1521b6d8fe61
shreyas bole <shreyasbole4@gmail.com>
2023-10-16 21:50:59 +0530

    question added

--- sorting/index.js
+++ sorting/index.js
@@ -203,3 +203,19 @@ const sortArr1 = (arr) => {
 };
 
 console.log(sortArr1([5, 2, 3, 1])); // [1, 2, 3, 5]
+
+// leetcode 1470. Shuffle the Array
+
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+const shuffle = (nums, n) => {
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(nums[i], nums[i + n]);
+  }
+  return result;
+};
+
+console.log(shuffle([2, 5, 1, 3, 4, 7], 3)); // [2, 3, 5, 4, 1, 7]

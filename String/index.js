ba632de0538e5fb1fd2bdabdff875e2dcfd01301
shreyas bole <shreyasbole4@gmail.com>
2023-11-23 23:36:37 +0530

    changes

--- String/index.js
+++ String/index.js
@@ -143,14 +143,3 @@ var isPalindrome = function (s) {
   }
   return true;
 };
-
-// TODO: question-7 : Reverse Words in a String III
-
-var reverseWords = function (s) {
-  let arr = s.split(" ");
-  let result = [];
-  for (let i = 0; i < arr.length; i++) {
-    result.push(arr[i].split("").reverse().join(""));
-  }
-  return result.join(" ");
-};

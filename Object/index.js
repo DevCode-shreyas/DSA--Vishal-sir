452512123929fb223f6bef371a157690ee3b5548
shreyas bole <shreyasbole4@gmail.com>
2024-01-19 23:28:36 +0530

    Add numJewelsInStones function to Object/index.js

--- Object/index.js
+++ Object/index.js
@@ -230,3 +230,15 @@ var defangIPaddr = function (address) {
   }
   return result;
 };
+
+// 771. Jewels and Stones
+
+var numJewelsInStones = function (jewels, stones) {
+  let count = 0;
+  for (let i = 0; i < stones.length; i++) {
+    if (jewels.includes(stones[i])) {
+      count++;
+    }
+  }
+  return count;
+};

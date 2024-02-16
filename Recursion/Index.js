82fd331f3075f18c1d4cea2dee8054478fc8e735
shreyas bole <shreyasbole4@gmail.com>
2024-02-16 22:59:20 +0530

    Add Tower of Hanoi implementation

--- Recursion/Index.js
+++ Recursion/Index.js
@@ -134,3 +134,17 @@ function permutations(str) {
 }
 
 console.log(permutations("123"));
+
+// Tower of Hanoi
+
+function towerOfHanoi(n, source, destination, auxiliary) {
+  if (n === 1) {
+    console.log(`Move disk 1 from ${source} to ${destination}`);
+    return;
+  }
+  towerOfHanoi(n - 1, source, auxiliary, destination);
+  console.log(`Move disk ${n} from ${source} to ${destination}`);
+  towerOfHanoi(n - 1, auxiliary, destination, source);
+}
+
+towerOfHanoi(3, "A", "C", "B");
